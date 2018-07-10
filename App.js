5c84e9c4744265a9c87eb427e931a91fe9701b93
Marcus Pontes <marcusporama@gmail.com>
2018-07-10 01:57:45 -0300

    finished project!

--- App.js
+++ App.js
@@ -1,8 +1,21 @@
 import React, { Component } from 'react';
-import { Text, StyleSheet, View, Image, TouchableOpacity, AppRegistry } from 'react-native';
+import { Text, StyleSheet, View, Image, TouchableOpacity, AppRegistry, Alert } from 'react-native';
 
-const botaoPrescionado = () =>{
-  alert('Botão prescionado')
+const gerarNovaFrase = () =>{
+  var numeroAleatorio = Math.random();
+  numeroAleatorio = Math.floor( numeroAleatorio * 5)
+
+var frases = Array();
+frases[0] = 'O dinheiro faz homens ricos, o conhecimento faz homens sábios e a humildade faz grandes homens.';
+frases[1] = 'Viva simples, sonhe grande, seja grato, dê amor, ria muito!';
+frases[2] = 'Bons amigos são como estrelas: nem sempre podemos ver,mas temos certeza que estão sempre lá.';
+frases[3] = 'Se for pra desistir, desista de ser fraco.';
+frases[4] = 'Que todo mal vire amor, que toda dor vire flor.';
+
+var fraseEscolhida = frases[ numeroAleatorio ];
+
+  Alert.alert( fraseEscolhida );
+
 }
 
 export default class App extends Component{
@@ -13,7 +26,9 @@ export default class App extends Component{
     return (
       <View style = { principal}>
         <Image source={require('./imgs/logo.png')} />
-        <TouchableOpacity style = { botao }>
+        <TouchableOpacity 
+        onPress={ gerarNovaFrase }
+          style = { botao }>
             <Text style = { textoBotao }>Nova frase </Text>
           </TouchableOpacity>
      </View>
